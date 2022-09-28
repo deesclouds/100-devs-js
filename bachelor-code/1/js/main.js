64d27be0e72cbd24d23ba5a99e515d8d2118f3b9
Darnell Davis <deescloud@protonmail.com>
2022-09-28 15:49:03 -0700

    add comments and wrote a function to hide photos

--- bachelor-code/1/js/main.js
+++ bachelor-code/1/js/main.js
@@ -1,6 +1,8 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+// when we click the h1 of Final Rose we run the function hide
+document.querySelector('#finalRose').addEventListener('click', hide)
 
+// this function hides claire and sharleen's photo
 function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+	document.querySelector('#claire').style.display = 'none'
+	document.querySelector('#sharleen').style.display = 'none'
 }
