f23b6d5363ffe1d1522c33f3d4cfcb954c250f3e
Darnell Davis <deescloud@protonmail.com>
2022-09-28 16:19:12 -0700

    complete function to find who received the final rose

--- bachelor-code/3/js/main.js
+++ bachelor-code/3/js/main.js
@@ -1,10 +1,15 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+// nikki got the final rose
+// when click on claire we want it to alert wrong
+// click on nikki we want her photo to display
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+const contestants = document.querySelectorAll('.contestant')
 
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
+
+// whatever we click on will become the click target
 function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+	if(click.target.classList.contains('rose')){
+		document.querySelector('#nikki').classList.toggle('hidden')
 	}else{
 		alert("Wrong!");
 	}
